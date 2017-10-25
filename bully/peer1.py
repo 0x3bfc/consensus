4117871c8a189b851fa1b267d1499e6e54072e79
seccuser <aabdallah@itida.gov.eg>
2017-10-25 18:36:37 +0200

    update peers

--- bully/peer1.py
+++ bully/peer1.py
@@ -1,12 +1,12 @@
 import peer
 
-# status = {host: binascii.hexlify(Random.get_random_bytes(30)) for host in hosts}
-status = {'peer3': b'4d46eb472348b8b22e1ac9d58d85615328cda5927d6431a2ac5131eca105',
+# tokens = {host: binascii.hexlify(Random.get_random_bytes(30)) for host in hosts}
+tokens = {'peer3': b'4d46eb472348b8b22e1ac9d58d85615328cda5927d6431a2ac5131eca105',
           'peer1': b'58661271ffbde96af93d368231eb49793aae9c53a3c32dbeba05cdb04f52',
           'peer2': b'eaee42eab023549b2285115b93e0b714b34f1888ff28b449071e042b1144'}
 
 peer.peer('peer1',
-          status,
+          tokens,
           'tcp://127.0.0.1',
           '4445',
           {'peer1': 'tcp://127.0.0.1:4445',
